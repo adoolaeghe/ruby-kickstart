715c30e61799da1e116ebfcefac31fb07f970f29
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-15 11:42:21 +0100

    Challenge1/exercise 6/7

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,15 @@
-# You'll get a string and a boolean.
+ # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  arr = string.split ("")
+    if return_odds == true then return arr.values_at(* arr.each_index.select{|e| e.odd?}).join
+    else arr.values_at(* arr.each_index.select{|e| e.even?}).join
+    end
 end
