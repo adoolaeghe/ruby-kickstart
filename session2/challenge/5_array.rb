650e2da99d2029afc543d8cec57f70b277e160a3
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-17 10:11:25 +0100

    update exercice 5_array.rb

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -14,11 +14,10 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
  def mod_three (numbers)
-   arr = numbers.each { |e|
-     if e % 3 == 0
-       numbers.delete(e)
-     else
-        e % 3
-    end
-  }
- end
+   def mod_three(nums)
+     to_return = []
+     nums.each do |n|
+         to_return << (n%3) unless n%3 == 0
+     end
+     return to_return
+ end 
