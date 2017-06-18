a56b6aaae58570a5b6b4520a7fefb03de3df702c
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-18 10:42:32 +0100

    Challenge 3/ exercice 5/

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -1,18 +1,30 @@
-# Write a method, spiral_access, that takes a 2d square array (an array of arrays)
-# and a block, and calls the block for each of the elements, in spiral order.
-#
-# HINT: This method is probably best sovled recursively
-#
-# Example:
-# two_d = [
-#   [ 1,  2,  3,  4, 5],
-#   [16, 17, 18, 19, 6],
-#   [15, 24, 25, 20, 7],
-#   [14, 23, 22, 21, 8],
-#   [13, 12, 11, 10, 9],
-# ]
-# order = []
-# spiral_access two_d do |i|
-#   order << i
-# end
-# order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+
+# After looking at the solution to get a general idea.
+# I went back to it the next day and managed to write a working one.
+
+
+ def spiral_access(ary, iteration=0, &block)
+     max_width = ary.length-1
+     max_height = ary.first.length-1
+
+     return if max_width/2 < iteration || max_height/2 < iteration
+
+     iteration.upto(max_width-iteration) do |n|
+         block.call(ary[iteration][n])
+     end
+
+     (iteration+1).upto(max_height-iteration) do |n|
+         block.call(ary[n][max_width-iteration])
+     end
+
+     (max_width-1-iteration).downto(iteration) do |n|
+        block.call(ary[max_height-iteration][n])
+     end
+
+     (max_height-1-iteration).downto(iteration+1) do |n|
+         block.call(ary[n][iteration])
+     end
+
+     spiral_access(ary, iteration+1, &block)
+ end
