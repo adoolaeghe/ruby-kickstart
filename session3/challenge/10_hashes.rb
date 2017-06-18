5a8171adff5c1e90718899f8b6c95ab369cbe65f
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-18 10:22:01 +0100

    Challenge 3/ Exercise 10/

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -1,33 +1,18 @@
-# Lets represent a file system with hashes
-# You will be passed a hash table, whose keys represent folders.
-# Their values will either be arrays of filenames in that directory
-# or they will be hashes with the same rules (a treelike structure)
-#
-# Your job is to take the hashes, and return an array containing
-# all of the complete file paths where each directory is separated by a '/'
-#
-# HINT:
-#   [1,2,3].is_a? Array # => true
-#   [1,2,3].is_a? Hash  # => false
-#   {1=>1}.is_a?  Array # => false
-#   {1=>1}.is_a?  Hash  # => true
-#
-# HINT2:
-#   Don't feel constrained, you may create any methods, classes, etc, that
-#   you need to you may address the problem in any way you need to (I
-#   haven't tried it yet, but will probably use a recursive approach)
-#
-# EXAMPLES:
-#
-# pathify 'usr' => {'bin' => ['ruby'] }                                                        # => ['/usr/bin/ruby']
-# pathify 'usr' => {'bin' => ['ruby', 'perl'] }                                                # => ['/usr/bin/ruby', '/usr/bin/perl']
-# pathify 'usr' => {'bin' => ['ruby'], 'include' => ['zlib.h'] }                               # => ['/usr/bin/ruby', '/usr/include/zlib.h']
-# pathify 'usr' => {'bin' => ['ruby']}, 'opt' => {'local' => {'bin' => ['sqlite3', 'rsync']} } # => ['/usr/bin/ruby', 'opt/local/bin/sqlite3', 'opt/local/bin/rsync']
-# pathify                                                                                      # => []
-#
-#
-# create it from scratch :)
 
 
-def pathify
-end
+# Couldn't solve this one out myself. I have made multiple tests for the solution, which were unhelpfull.
+# I am not sure I have well understood the recursive logic. I would like to return to this in week 3.  
+
+ def pathify(dir=Hash.new)
+     return dir.map{|i| "/#{i}"} if dir.is_a? Array
+     to_return = []
+     dir.each do |k, v|
+         working_directory = "/#{k}"
+         p working_directory
+         paths = pathify(v)
+         paths.each do |path|
+             to_return << (working_directory + path)
+         end
+     end
+     to_return
+ end
