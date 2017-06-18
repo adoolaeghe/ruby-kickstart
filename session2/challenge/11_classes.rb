481a9fb06a7fa2fa26a92b4473ee9d0272c53491
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-18 12:06:17 +0100

    Challenge 3/ Exercice11/

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,40 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+   def initialize(num_bottles)
+     @num_bottles = [[0,num_bottles].max,99].min
+   end
+
+   def print_song
+     counter = @num_bottles
+     while counter > 0
+     	puts "#{bottles(counter)} of beer on the wall,"
+       puts "#{bottles(counter)} of beer,"
+     	puts "Take one down, pass it around,"
+       puts "#{bottles(counter-1)} of beer on the wall."
+     	counter -= 1
+     end
+   end
+
+   private
+
+   def bottles(num)
+   	literals = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+   	tens = ["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+   	teens = ["","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+
+     return "One bottle" if num == 1
+     return literals[num].capitalize + " " + "bottles" if num < 10
+     return tens[num/10].capitalize + " " + "bottles" if num % 10 == 0
+     return teens[num%10].capitalize + " " + "bottles" if num < 20
+     return tens[num/10].capitalize + "-" + literals[num%10] + " " + "bottles"
+   end
+ end
